0dd992b0fef5c320ecdbbec5a794407f50636d92
fox5352 <christophervos522@gmail.com>
2025-06-17 23:48:04 +0200

    added tests for CustomShape struct methods

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -41,7 +41,49 @@ pub mod shapes {
     }
 }
 
-// mod for cal colitions
-pub mod collision_cal {
-    use crate::units::{Point, Size};
+#[cfg(test)]
+mod test_shapes {
+    use super::shapes::CustomShape;
+
+    #[test]
+    fn test_gen_rectangle() {
+        let shape = CustomShape::gen_rectangle();
+        assert_eq!(
+            shape.points,
+            vec![(0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)]
+        );
+    }
+
+    #[test]
+    fn test_gen_triangle() {
+        let shape = CustomShape::gen_triangle();
+        assert_eq!(
+            shape.points,
+            vec![(0.0, 0.0), (0.5, 1.0), (1.0, 0.0), (0.0, 0.0)]
+        );
+    }
+
+    #[test]
+    fn test_add_point() {
+        let mut shape = CustomShape::gen_rectangle();
+        shape.add_point((2.0, 2.0));
+        assert_eq!(
+            shape.points,
+            vec![
+                (0.0, 0.0),
+                (0.0, 1.0),
+                (1.0, 1.0),
+                (1.0, 0.0),
+                (0.0, 0.0),
+                (2.0, 2.0)
+            ]
+        );
+    }
+
+    #[test]
+    fn test_override_points() {
+        let mut shape = CustomShape::gen_rectangle();
+        shape.override_points(vec![(2.0, 2.0)]);
+        assert_eq!(shape.points, vec![(2.0, 2.0)]);
+    }
 }
